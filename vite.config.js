dc55643896d8f3b245c749d6948f94f590e9a0ab
pycgamingstudio-sys <pycgamingstudio@gmail.com>
2026-09-07 08:17:00 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 export default defineConfig({
   plugins: [react()],
   resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
     alias: {
       "@": path.resolve(__dirname, "./"),
       "src": path.resolve(__dirname, "./"),
