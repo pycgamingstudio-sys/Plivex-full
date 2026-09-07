31e546316c581c80832492bd3bb9b11cbd0a46bb
pycgamingstudio-sys <pycgamingstudio@gmail.com>
2026-09-07 08:02:01 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,8 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "./"),
+      "src": path.resolve(__dirname, "./"),
     },
   },
   build: {
